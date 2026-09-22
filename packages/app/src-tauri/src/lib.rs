d3a4bc6050742104b94a6382e8be0cad8cfb5e23
maryvellous <mary.dembech@gmail.com>
2026-09-22 04:02:23 +0200

    release: remove updater runtime plugins

--- packages/app/src-tauri/src/lib.rs
+++ packages/app/src-tauri/src/lib.rs
@@ -441,8 +441,6 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
-        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_http::init())
         .invoke_handler(tauri::generate_handler![
             confirm_close,
